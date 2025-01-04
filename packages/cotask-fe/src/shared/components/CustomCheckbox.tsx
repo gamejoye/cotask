634986ac7409bd4a9b904ef630aec9a69af6f61e
gamejoye <gamejoye@gmail.com>
2025-01-04 22:32:39 +0800

    feat(shared): 自定义多选框

---
+++ packages/cotask-fe/src/shared/components/CustomCheckbox.tsx
@@ -0,0 +1,62 @@
+import { Typography } from "antd";
+import useToken from "antd/es/theme/useToken";
+import { useState } from "react";
+
+export type Props = {
+  options: {
+    key: string;
+    label: string;
+    value: string;
+  }[];
+  onChange?: (checkedValues: string[]) => void;
+}
+
+export default function CustomCheckboxGrid({ options, onChange }: Props) {
+  const { colorBgBase, colorPrimary, colorBorder } = useToken()[1];
+  const [selecteds, setSelecteds] = useState<string[]>([]);
+
+  const toggle = (day: string) => {
+    const newSelected = selecteds.includes(day) ? selecteds.filter(d => d !== day) : [...selecteds, day];
+    setSelecteds(newSelected);
+    onChange && onChange(newSelected);
+  };
+
+  return (
+    <div style={gridContainerStyle}>
+      {options.map(({ key, value, label }) => (
+        <div
+          key={key}
+          onClick={() => toggle(value)}
+          style={{
+            ...gridItemStyle,
+            backgroundColor: selecteds.includes(value) ? colorPrimary : colorBgBase,
+            border: `1px solid ${colorBorder}`,
+            cursor: 'pointer'
+          }}
+        >
+          <Typography.Text type='secondary'>{label}</Typography.Text>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const gridContainerStyle: React.CSSProperties = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(7, 40px)',
+  gap: '8px',
+  justifyContent: 'center'
+};
+
+const gridItemStyle: React.CSSProperties = {
+  width: '40px',
+  height: '40px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '4px',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  userSelect: 'none',
+  transition: 'all 0.2s ease-in-out'
+};
---
+++ packages/cotask-fe/src/shared/components/stories/CustomCheckbox.stories.tsx
@@ -0,0 +1,42 @@
+import { fn } from '@storybook/test';
+
+import CustomCheckbox from '../CustomCheckbox';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Shared/CubtomCheckbox',
+  component: CustomCheckbox,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+};
+
+const Template = (args: any) => <CustomCheckbox {...args} />;
+
+export const Default = Template.bind({});
+(Default as any).args = {
+  options: [{
+    key: '1',
+    label: '1',
+    value: '1',
+  }, {
+    key: '2',
+    label: '2',
+    value: '2',
+  }, {
+    key: '3',
+    label: '3',
+    value: '3',
+  }, {
+    key: '4',
+    label: '4',
+    value: '4',
+  }, ],
+  onChange: fn()
+}
+
+
