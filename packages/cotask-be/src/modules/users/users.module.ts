b0940a499b44b2f577ca717da70d05f2d347b9f9
gamejoye <gamejoye@gmail.com>
2025-01-09 18:49:07 +0800

    feat(users): 导入database模块

--- packages/cotask-be/src/modules/users/users.module.ts
+++ packages/cotask-be/src/modules/users/users.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 import { IUsersService } from './services/users.abstract';
+import { DATA_SOURCE, USER_REPO } from '@cotask-be/common/constans/table-repos';
+import { DataSource } from 'typeorm';
+import { User } from './entities/user.entity';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [
     {
       provide: IUsersService,
       useClass: UsersService,
+    },
+    {
+      provide: USER_REPO,
+      useFactory: (dataSource: DataSource) => dataSource.getRepository(User),
+      inject: [DATA_SOURCE],
     },
   ],
   controllers: [UsersController],
