4e5a10868aac50cf5ba431c4cc43c4bf97882812
gamejoye <gamejoye@gmail.com>
2025-01-10 17:14:54 +0800

    feat(cotask-be): import TodosModule

--- packages/cotask-be/src/app.module.ts
+++ packages/cotask-be/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/auth';
 import { UsersModule } from './modules/users';
+import { TodosModule } from './modules/todos';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, TodosModule],
 })
 export class AppModule {}
