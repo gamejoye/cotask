382391afe2b6088e5f670fa94428fdf06612746a
gamejoye <gamejoye@gmail.com>
2025-01-10 17:08:33 +0800

    build(types): 打包方式

---
+++ packages/types/build-types.js
@@ -0,0 +1,37 @@
+import { build } from 'vite';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFileName = (format, ext) => {
+  return {
+    es: `index${ext}.mjs`,
+    cjs: `index${ext}.cjs`,
+  }[format];
+};
+
+const entrys = [
+  {
+    entry: path.join(__dirname, 'src/index.ts'),
+    fileName: format => getFileName(format, ''),
+  },
+];
+
+entrys.forEach(({ entry, fileName }) => {
+  build({
+    build: {
+      outDir: path.join(__dirname, 'dist'),
+      target: 'esnext',
+      emptyOutDir: false,
+      lib: {
+        entry,
+        name: '@cotask/types',
+        formats: ['es', 'cjs'],
+        fileName: format => fileName(format),
+      },
+      sourcemap: true,
+    },
+  });
+});
