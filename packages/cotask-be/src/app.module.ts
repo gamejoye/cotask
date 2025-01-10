1a5fd64a846b38c7adefce4ef6b03c53c9e3b59b
gamejoye <gamejoye@gmail.com>
2025-01-10 20:50:30 +0800

    feat(cotask-be): import Groups/UsersGroups Module

--- packages/cotask-be/src/app.module.ts
+++ packages/cotask-be/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth';
 import { UsersModule } from './modules/users';
 import { TodosModule } from './modules/todos';
+import { GroupsModule } from './modules/groups';
+import { UsersGroupsModule } from './modules/users-groups';
 
 @Module({
-  imports: [AuthModule, UsersModule, TodosModule],
+  imports: [AuthModule, UsersModule, TodosModule, GroupsModule, UsersGroupsModule],
 })
 export class AppModule {}
