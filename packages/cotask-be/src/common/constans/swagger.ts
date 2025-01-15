bbc4b74ec950b14e391ede7e51988e09c02fb56d
gamejoye <gamejoye@gmail.com>
2025-01-15 22:01:40 +0800

    feat(common): swagger常量

---
+++ packages/cotask-be/src/common/constans/swagger.ts
@@ -0,0 +1,4 @@
+export const swaggerTitle = 'Cotask Backend Api';
+export const documentDescription = 'Cotask: 一款面向团队的协同待办事项工具';
+export const documentVersion = 'v0.0.0';
+export const documentTag = 'Api/V1';
