fadfdd4ba095b224bc6d2b2fa1a0003193f889e5
gamejoye <gamejoye@gmail.com>
2025-01-24 03:31:03 +0800

    feat: frequencyOption类型完备

--- packages/types/src/base.ts
+++ packages/types/src/base.ts
@@ -1,3 +1,5 @@
+import { components } from './generated/openapi';
+
 export enum PriorityTypes {
   HIGH = 'HIGH',
   MEDIUM = 'MEDIUM',
@@ -12,43 +14,15 @@ export enum FrequencyTypes {
   YEARLY = 'YEARLY',
 }
 
-export type Todo = {
-  id: number;
-  title: string;
-  completed: boolean;
-  frequency: FrequencyTypes;
-  frequencyOptions?: FrequencyOptions;
-  priority?: PriorityTypes;
-  dueDate: string;
-  createAt: string;
-};
+export type Todo = components['schemas']['TodoVo'];
+export type Group = components['schemas']['GroupVo'];
 
-export type Group = {
-  id: number;
-  name: string;
-};
+export const frequencyTypes = [
+  FrequencyTypes.NONE,
+  FrequencyTypes.DAILY,
+  FrequencyTypes.WEEKLY,
+  FrequencyTypes.MONTHLY,
+  FrequencyTypes.YEARLY,
+] as const;
 
-export const frequencyTypes = ['NONE', 'DAILY', 'WEEKLY', 'MONTHLY', 'YEARLY'] as const;
-// export type FrequencyTypes = (typeof frequencyTypes)[number];
-
-export type DailyPickerProps = {
-  onDaysChange: (val: number) => void;
-};
-
-export type WeeklyPickerProps = {
-  onWeeksChange: (val: { weeks: number; selectDays: number[] }) => void;
-};
-
-export type MonthlyPickerProps = {
-  onMonthsChange: (val: { months: number; selectDays: number[] }) => void;
-};
-
-export type YearlyPickerProps = {
-  onYearsChange: (val: { years: number; selectMonths: number[] }) => void;
-};
-
-export type FrequencyOptions =
-  | { type: 'DAILY'; options: Parameters<DailyPickerProps['onDaysChange']>[0] }
-  | { type: 'WEEKLY'; options: Parameters<WeeklyPickerProps['onWeeksChange']>[0] }
-  | { type: 'MONTHLY'; options: Parameters<MonthlyPickerProps['onMonthsChange']>[0] }
-  | { type: 'YEARLY'; options: Parameters<YearlyPickerProps['onYearsChange']>[0] };
+export type FrequencyOptions = components['schemas']['TodoVo']['frequencyOption'];
