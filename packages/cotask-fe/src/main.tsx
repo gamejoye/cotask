2304ac1b5d9075f71ff7324ac7c6a4c9eede8b17
gamejoye <gamejoye@gmail.com>
2025-01-26 02:23:34 +0800

    fix(cotask-fe): body未覆盖整个页面

--- packages/cotask-fe/src/main.tsx
+++ packages/cotask-fe/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import routers from './routes';
 import { StrictMode } from 'react';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
