b98747dec568be51d033046bfb6d9d27a7f356b5
gamejoye <gamejoye@gmail.com>
2025-02-02 23:28:02 +0800

    feat(pages): Dashboard展示TodoList && 新建Group按钮

--- packages/cotask-fe/src/pages/Dashboard.tsx
+++ packages/cotask-fe/src/pages/Dashboard.tsx
@@ -1,20 +1,30 @@
+import { PlusOutlined } from '@ant-design/icons';
 import GroupList from '@cotask-fe/modules/group/components/GroupList';
 import { useGroup } from '@cotask-fe/modules/group/hooks';
 import TodoList from '@cotask-fe/modules/todo/components/TodoList';
 import { useTodo } from '@cotask-fe/modules/todo/hooks';
 import CotaskCard from '@cotask-fe/shared/components/CotaskCard';
+import CotaskLogo from '@cotask-fe/shared/components/CotaskLogo';
 import { Group } from '@cotask/types';
-import { Divider, Layout, theme } from 'antd';
+import { Button, Layout, theme, Typography } from 'antd';
 import { useState } from 'react';
 
-const { Sider, Header, Content } = Layout;
+const { Sider, Header, Content, Footer } = Layout;
+
+const siderStyle: React.CSSProperties = {
+  height: '100vh',
+  position: 'fixed',
+  insetInlineStart: 0,
+  top: 0,
+  bottom: 0,
+};
 
 export default function Dashboard() {
   const { groups, loading: groupsLoading, error: groupsError } = useGroup();
   const [selectedGroup, setSelectedGroup] = useState<Group | null>(null);
   const { todos } = useTodo(selectedGroup);
   const {
-    token: { colorBgContainer },
+    token: { colorBgContainer, colorFillContent },
   } = theme.useToken();
 
   return (
@@ -22,41 +32,85 @@ export default function Dashboard() {
       <Sider
         width={280}
         style={{
+          ...siderStyle,
           background: colorBgContainer,
           padding: 20,
         }}
       >
-        <div
+        <Layout
           style={{
-            display: 'grid',
-            gap: '12px',
+            backgroundColor: colorBgContainer,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
           }}
         >
-          <CotaskCard title='我的' content='3项待完成' active onClick={() => {}} />
-          <CotaskCard title='全部' content='12项待完成' onClick={() => {}} />
-        </div>
-        <Divider />
-        <GroupList
-          groups={groups}
-          loading={groupsLoading}
-          error={groupsError}
-          onClick={group => {
-            setSelectedGroup(group);
-          }}
-          hasMore={false}
-          loadMore={() => {
-            console.log('loadMore');
-          }}
-        />
+          <Header style={{ display: 'flex', justifyContent: 'center' }}>
+            <CotaskLogo size='small' />
+          </Header>
+          <Content
+            style={{
+              flex: 1,
+              overflow: 'scroll',
+              scrollbarWidth: 'none',
+            }}
+          >
+            <div
+              style={{
+                display: 'grid',
+                gap: '12px',
+              }}
+            >
+              <CotaskCard title='我的' content='3项待完成' active onClick={() => {}} />
+              <CotaskCard title='全部' content='12项待完成' onClick={() => {}} />
+            </div>
+            <GroupList
+              groups={groups}
+              loading={groupsLoading}
+              error={groupsError}
+              onClick={group => {
+                setSelectedGroup(group);
+              }}
+              hasMore={false}
+              loadMore={() => {
+                console.log('loadMore');
+              }}
+            />
+          </Content>
+          <Footer
+            style={{
+              backgroundColor: colorBgContainer,
+              paddingLeft: 0,
+              paddingRight: 0,
+              marginTop: 'auto',
+            }}
+          >
+            <Button
+              icon={<PlusOutlined />}
+              onClick={() => {
+                /** TODO: 创建新的Group */
+              }}
+              type='dashed'
+              size='large'
+              style={{ width: '100%' }}
+            >
+              新建群组
+            </Button>
+          </Footer>
+        </Layout>
       </Sider>
-      <Layout>
+      <Layout style={{ backgroundColor: colorBgContainer, marginInlineStart: 280 }}>
         <Header style={{ paddingLeft: 20, backgroundColor: colorBgContainer }}>
-          <div style={{ fontSize: 24, fontWeight: 600 }}>当前群组任务</div>
+          <Typography.Title level={2}>当前群组任务</Typography.Title>
         </Header>
         <Content
           style={{
-            margin: '0px 16px',
+            margin: '32px 16px',
             padding: 24,
+            backgroundColor: colorFillContent,
+            borderRadius: 8,
+            overflow: 'scroll',
+            scrollbarWidth: 'none',
           }}
         >
           {selectedGroup ? (
