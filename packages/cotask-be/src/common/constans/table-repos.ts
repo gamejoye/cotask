849270f404983e5acda815674bb5f3fd81866e05
gamejoye <gamejoye@gmail.com>
2025-02-05 03:23:21 +0800

    feat(common): USERS_GROUPS_REPO常量

--- packages/cotask-be/src/common/constans/table-repos.ts
+++ packages/cotask-be/src/common/constans/table-repos.ts
@@ -2,3 +2,4 @@ export const DATA_SOURCE = 'dataSource';
 export const USER_REPO = 'userRepo';
 export const TODO_REPO = 'todoRepo';
 export const GROUP_REPO = 'groupRepo';
+export const USERS_GROUPS_REPO = 'userGroupRepo';
