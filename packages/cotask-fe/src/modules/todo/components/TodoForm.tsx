b9c95ea7d74cd25549ad8015506cf16b08930289
gamejoye <gamejoye@gmail.com>
2025-02-07 18:11:09 +0800

    feat(components): TodoForm集成ai

--- packages/cotask-fe/src/modules/todo/components/TodoForm.tsx
+++ packages/cotask-fe/src/modules/todo/components/TodoForm.tsx
@@ -1,12 +1,24 @@
-import { Form, Input, Button, Select, DatePicker } from 'antd';
+import {
+  Form,
+  Input,
+  Button,
+  Select,
+  DatePicker,
+  AutoComplete,
+  AutoCompleteProps,
+  Tag,
+} from 'antd';
 import { useForm } from 'antd/es/form/Form';
 import Modal from 'antd/es/modal/Modal';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import CotaskDatePicker from '@cotask-fe/shared/components/CotaskDatePicker';
 import { FrequencyOptions, FrequencyTypes, frequencyTypes } from '@cotask/types';
 import dayjs from 'dayjs';
 import { useLastState } from '@cotask-fe/shared/hooks';
 import { Todo } from '@cotask-fe/shared/models';
+import { useAiTodoTitle } from '../hooks';
+import { AlertOutlined } from '@ant-design/icons';
+import { debounce } from '@cotask/utils';
 
 export type Props = {
   todo: Todo;
@@ -26,6 +38,39 @@ const formItemLayout = {
 
 type FormValues = Pick<Todo, 'title' | 'priority' | 'dueDate' | 'frequency'>;
 
+type AutoCompleteLabelProps = {
+  value: string;
+};
+
+function AutoCompleteLabel({ value }: AutoCompleteLabelProps) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        gap: 8,
+        padding: '4px 0',
+        maxWidth: '100%',
+      }}
+    >
+      <div
+        style={{
+          wordBreak: 'break-all',
+          whiteSpace: 'pre-wrap',
+          lineHeight: '1.5',
+          flex: 1,
+        }}
+      >
+        {value}
+      </div>
+      <Tag color='#108ee9' icon={<AlertOutlined />}>
+        AI建议
+      </Tag>
+    </div>
+  );
+}
+
 export default function TodoForm({ todo, onEdit }: Props) {
   const [form] = useForm<Todo>();
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -33,6 +78,20 @@ export default function TodoForm({ todo, onEdit }: Props) {
   const [frequency, setFrequency, lastFrequency] = useLastState<FrequencyTypes>(
     FrequencyTypes.NONE
   ); // 用于控制CotaskDatePicker的initialFrequency
+  const [suggestTodo, setSuggestTodo] = useState<Todo>(todo);
+  const { data: suggestions, loading } = useAiTodoTitle(suggestTodo);
+
+  const titleOptions = useMemo<AutoCompleteProps['options']>(() => {
+    return suggestions.map(s => ({
+      value: s,
+      label: <AutoCompleteLabel value={s} />,
+      disabled: loading,
+    }));
+  }, [suggestions, loading]);
+
+  const onTitleChange = debounce((value: string) => {
+    setSuggestTodo({ ...todo, title: value });
+  }, 1000);
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -80,7 +139,13 @@ export default function TodoForm({ todo, onEdit }: Props) {
           name='title'
           rules={[{ required: true, message: '请输入标题！' }]}
         >
-          <Input placeholder='输入待办事项标题' variant='borderless' />
+          <AutoComplete
+            options={titleOptions}
+            onChange={onTitleChange}
+            placeholder='输入待办事项标题'
+          >
+            <Input />
+          </AutoComplete>
         </Form.Item>
 
         <Form.Item<FormValues>
