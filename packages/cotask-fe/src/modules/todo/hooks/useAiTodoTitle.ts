62f34904b83f3dcce10f1a4f7fe8697f491fa72f
gamejoye <gamejoye@gmail.com>
2025-02-08 09:57:39 +0800

    feat(hooks): 记忆化api

--- packages/cotask-fe/src/modules/todo/hooks/useAiTodoTitle.ts
+++ packages/cotask-fe/src/modules/todo/hooks/useAiTodoTitle.ts
@@ -1,16 +1,22 @@
 import { useRequest } from 'ahooks';
 import { aiCompleteTodoTitleApi } from '../apis/ai-complete-todo-title';
 import { Todo } from '@cotask-fe/shared/models';
+import { memoized } from '@cotask/utils';
+
+const memoizedAiCompleteTodoTitleApi = memoized(aiCompleteTodoTitleApi, {
+  expirationTime: 1000 * 60, // 1分钟
+});
 
 export function useAiTodoTitle(todo: Todo) {
   const {
     data: res,
     loading,
     run,
-  } = useRequest(aiCompleteTodoTitleApi, {
+  } = useRequest(memoizedAiCompleteTodoTitleApi, {
+    defaultParams: [todo],
     refreshDeps: [todo.title, todo.description],
     refreshDepsAction: () => {
-      if (!todo.title && !todo.description) {
+      if ((!todo.title && !todo.description) || (res?.data ?? []).includes(todo.title)) {
         return;
       }
       run(todo);
